c96afc577901eb021db839615f9cd96c4d7f1a26
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-09-09 18:31:35 +0530

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Lahiru Amarasooriya on 2023-09-09.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
